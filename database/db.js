b51616c139e2adef301e37b70bf9b9a428c69113
Momo Yang <momo.yang666@gmail.com>
2023-11-22 13:09:02 +0100

    enable everyone to configure their own db settings

--- database/db.js
+++ database/db.js
@@ -3,9 +3,9 @@ const { Pool } = require("pg");
 const pool = new Pool({
   host: "localhost",
   port: 5432,
-  user: "postgres",
-  password: "postgres",
-  database: "migracode_final_project"
+  user: process.env.DB_USERNAME || "postgres",
+  password: process.env.DB_PASSWORD || "postgres",
+  database: process.env.DB_NAME || "migracode_final_project",
 });
 
 //   user: process.env.DB_PASSWORD || "postgres",
