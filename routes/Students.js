80ff1113bd0d64a3ea7cd93f3b9cbc394589e059
Bonnie Mo <knockri-am-9@Bonnies-MacBook-Air.local>
2023-11-22 14:55:47 -0500

    remove fetch student images from backend

--- routes/Students.js
+++ routes/Students.js
@@ -60,21 +60,21 @@ router.get("/", async (req, res) => {
         // offset for next page
         let allData = { items: [], offset: data.offset };
 
-        // promise to fetch images
-        await Promise.all(
-            // each record
+        // // promise to fetch images
+        // await Promise.all(
+        //     // each record
             data.records.map(async (record) => {
-                let avatar_url = "";
-                try {
-                    // 1) fetch image link
-                    const githubResponse = await axios.get(
-                        `https://api.github.com/users/${record.fields["GitHub handle"]}`
-                    );
-                    // set image in variable
-                    avatar_url = githubResponse.data.avatar_url;
-                } catch (error) {
-                    console.error(error.message);
-                }
+        //         let avatar_url = "";
+        //         try {
+        //             // 1) fetch image link
+        //             const githubResponse = await axios.get(
+        //                 `https://api.github.com/users/${record.fields["GitHub handle"]}`
+        //             );
+        //             // set image in variable
+        //             avatar_url = githubResponse.data.avatar_url;
+        //         } catch (error) {
+        //             console.error(error.message);
+        //         }
 
                 allData.items.push({
                     id: record.id,
@@ -85,7 +85,7 @@ router.get("/", async (req, res) => {
                     Languages: record.fields["Language(s)"],
                     gender: record.fields.Gender,
                     gitHub: record.fields["GitHub handle"],
-                    imageUrl: avatar_url,
+                    imageUrl: ``,
                     group: record.fields.Group,
                     LinkedIn: record.fields.LinkedIn
                         ? record.fields.LinkedIn
@@ -96,7 +96,7 @@ router.get("/", async (req, res) => {
                     courseCertificate: record.fields["Course certificate"],
                 });
             })
-        );
+        // );
         return res.json(allData);
     } catch (error) {
         console.log(error);
