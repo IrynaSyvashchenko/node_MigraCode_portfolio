c5dabcca3df297b7e4702f0e829ceb3a82d316ea
Iryna S <byyylafka@ukr.net>
2023-11-23 09:45:30 +0100

    Database

--- database/db.js
+++ database/db.js
@@ -1,23 +1,24 @@
 const { Pool } = require("pg");
-
-const pool = new Pool({
-  host: "localhost",
-  port: 5432,
-  user: "postgres",
-  password: "postgres",
-  database: "migracode_final_project"
-});
-
-//   user: process.env.DB_PASSWORD || "postgres",
-//   password: process.env.DB_PASSWORD || "bygaga",
-//   database: "portfolio",
+require("dotenv").config();
 
 // const pool = new Pool({
 //   host: "localhost",
 //   port: 5432,
 //   user: "postgres",
-//   password: "bygaga",
-//   database: "portfolio",
+//   password: "postgres",
+//   database: "migracode_final_project"
 // });
+
+//   user: process.env.DB_PASSWORD || "postgres",
+//   password: process.env.DB_PASSWORD || "bygaga",
+//   database: "portfolio",
+
+const pool = new Pool({
+  host: "db.byvzdpgaevzslrfcvbig.supabase.co",
+  port: 5432,
+  user: "postgres",
+  password: process.env.DB_PASSWORD,
+  database: "postgres",
+});
 
 module.exports = pool;
